433cb3cb6178c3aff82bcef52f276ec468c88a46
FireKun8 <aleixribastorras@gmail.com>
2024-09-24 21:17:43 +0200

    feat: Added plot

--- Assignment 1/SymSolver.py
+++ Assignment 1/SymSolver.py
@@ -8,7 +8,7 @@ class SymSolver:
         self.L, self.A, self.E, self.F = cParams['L'], cParams['A'], cParams['E'], cParams['F']
         self.g, self.r = cParams['g'], cParams['r']
         self.u = Function('u', real=True)
-        self.rho = pi / self.L**2
+        self.rho = pi**3 / self.L**2
         self.s = self.g * pi**4 / self.L**2
         self.q = self.E * (self.rho * self.u(x) - self.s * self.r)
     
@@ -18,7 +18,7 @@ class SymSolver:
         f = self.q / self.E
         EDO = upp + f
         EDO = EDO.subs(self.r, (x / self.L)**2)
-        solution = dsolve(EDO, self.u(x), ics={self.u(0): -self.g, self.u(x).diff(x).subs(x,self.L): self.F / (self.A * self.E)})
+        solution = dsolve(EDO, self.u(x), ics={self.u(0): -self.g, self.u(x).diff(x).subs(x,self.L): pi**2 * self.g / self.L})
         return(solution)
     
     def numSolve(self):
--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,8 +1,8 @@
-
-import math
-from sympy import Symbol
+from sympy import Symbol, lambdify
 from sympy.abc import x
 from SymSolver import SymSolver
+import numpy as np
+import matplotlib.pyplot as plt
 
 cParams = {
     'F': Symbol('F', real = True),
@@ -15,4 +15,11 @@ cParams = {
 
 SolverProb1 = SymSolver(cParams)
 solution = SolverProb1.numSolve().simplify().evalf()
+solution = solution.rhs
 print(solution)
+
+num_solution = lambdify(x, solution, 'numpy')
+x_vals = np.linspace(0, 1, 1000)
+y_vals = num_solution(x_vals)
+plt.plot(x_vals, y_vals)
+plt.show()
