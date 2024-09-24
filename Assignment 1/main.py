f2af7747eba37031389c4a24d7d6847b2db5caa4
FireKun8 <aleixribastorras@gmail.com>
2024-09-24 11:55:53 +0200

    feat: Added the constructor

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,6 +1,20 @@
 import math
-from sympy import Symbol
+from sympy import *
 
 class SymSolver:
-    def __init__(self):
-        self.L = Symbol("L")
+
+    def __init__(self, r):
+        self.x = Symbol('x')
+        self.L = Symbol('L', positive=True)
+        self.F = Symbol('F')
+        self.E = Symbol('E', positive=True)
+        self.A = Symbol('A', positive=True)
+        self.g = Symbol('g')
+        self.rho = pi / self.L**2
+        self.s = self.g * pi**4 / self.L**2
+        u = Function('u', real=True)
+        self.q = self.E * (self.rho * u )
+
+
+
+solver = SymSolver(2)
