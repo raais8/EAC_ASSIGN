427a5d60550bc6b5b20f65aa45272749f56c85bb
raais8 <aleixribastorras@gmail.com>
2024-10-16 08:50:58 +0200

    feat: Solver wip

--- Assignment 1/SymSolver.py
+++ Assignment 1/SymSolver.py
@@ -1,12 +1,14 @@
 import math
-from sympy import Symbol, pi, Function, diff, dsolve
+from sympy import Symbol, pi, Function, diff, dsolve, integrate
 from sympy.abc import x
+import numpy as np
 
 class ExactSolver:
 
     def __init__(self, cParams):
         self.L, self.A, self.E, self.F = cParams['L'], cParams['A'], cParams['E'], cParams['F']
         self.g, self.r = cParams['g'], cParams['r']
+        self.N = cParams['N']
         self.u = Function('u', real=True)
         self.rho = pi**3 / self.L**2
         self.s = self.g * pi**4 / self.L**2
@@ -25,3 +27,19 @@ class ExactSolver:
         solution = self.buildAndSolveODE()
         numSolution = solution.subs({self.L: 1, self.g: 0.01})
         return(numSolution.rhs)
+    
+    def galerkinMethod(self):
+        N_L = self.N.subs(x, self.L)
+        B = diff(self.N, x)
+        K1 = integrate(B.T * B, (x, 0, self.L))
+        K2 = integrate((self.rho * self.N.T * self.N), (x, 0, self.L))
+        K = K1 - K2
+        F1 = integrate(self.N.T * self.s * self.r, (x, 0, self.L))
+        F = pi**2 * self.g / self.L * N_L.T - F1
+        r = [0]
+        l = np.arange(1, self.N.shape[1]).tolist()
+        KK = K.extract(l, l)
+        FF = (F.extract(l,[0]) - K.extract(l, r) * self.g)
+        dl = KK.LUsolve(FF)
+        u = self.g + self.N.extract(l,[0]) * dl
+        return(u)
--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,4 +1,4 @@
-from sympy import Symbol, lambdify
+from sympy import Symbol, lambdify, Matrix, S
 from sympy.abc import x
 from SymSolver import ExactSolver
 import numpy as np
@@ -10,30 +10,35 @@ cParams = {
     'L': Symbol('L', positive = True),
     'A': Symbol('A', positive = True),
     'E': Symbol('E', positive = True),
-    'r': Symbol('r', positive = True),  
+    'r': Symbol('r', positive = True),
+    'N': Matrix([[S(1), x, x**2]]),
 }
 
-SolverProb1 = ExactSolver(cParams)
-solution = SolverProb1.numSolve().simplify().evalf()
+# SolverProb1 = ExactSolver(cParams)
+# solution = SolverProb1.numSolve().simplify().evalf()
+# print(solution)
+
+# num_solution = lambdify(x, solution, 'numpy')
+# x_vals = np.linspace(0, 1, 1000)
+# y_vals = num_solution(x_vals)
+# plt.plot(x_vals, y_vals,linewidth = 1.5)
+# plt.title(r'$\mathbf{Axial\, displacements}$', loc = 'center', fontsize=14)
+# plt.xlabel(r'$\mathbf{Position\,(m)}$', loc = 'center', fontsize = 12)
+# plt.ylabel(r'$\mathbf{Displacement\,(m)}$', loc = 'center', fontsize = 12)
+# plt.grid(True, linewidth = 1.5)
+# plt.tick_params(axis = 'both', which = 'major', labelsize = 11)
+# plt.gca().spines['top'].set_linewidth(1.5)
+# plt.gca().spines['right'].set_linewidth(1.5)  
+# plt.gca().spines['left'].set_linewidth(1.5) 
+# plt.gca().spines['bottom'].set_linewidth(1.5)
+# plt.xlim(0,1)
+# plt.ylim(-0.015,0.025)
+
+# plt.show()
+
+SolverProb2 = ExactSolver(cParams)
+solution = SolverProb2.galerkinMethod()
 print(solution)
-
-num_solution = lambdify(x, solution, 'numpy')
-x_vals = np.linspace(0, 1, 1000)
-y_vals = num_solution(x_vals)
-plt.plot(x_vals, y_vals,linewidth = 1.5)
-plt.title(r'$\mathbf{Axial\, displacements}$', loc = 'center', fontsize=14)
-plt.xlabel(r'$\mathbf{Position\,(m)}$', loc = 'center', fontsize = 12)
-plt.ylabel(r'$\mathbf{Displacement\,(m)}$', loc = 'center', fontsize = 12)
-plt.grid(True, linewidth = 1.5)
-plt.tick_params(axis = 'both', which = 'major', labelsize = 11)
-plt.gca().spines['top'].set_linewidth(1.5)
-plt.gca().spines['right'].set_linewidth(1.5)  
-plt.gca().spines['left'].set_linewidth(1.5) 
-plt.gca().spines['bottom'].set_linewidth(1.5)
-plt.xlim(0,1)
-plt.ylim(-0.015,0.025)
-
-plt.show()
 
 
 
